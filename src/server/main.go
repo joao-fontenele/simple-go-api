6b429fd02586aeecd9b70a5186dc112f50dc2203
João Paulo Fontenele Brito <jpfb@icomp.ufam.edu.br>
2018-09-19 18:17:37 -0400

    refactor(server): refactor code into cascading multiple handlers

--- src/server/main.go
+++ src/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -17,10 +18,54 @@ type helloWorldRequest struct {
 	Name string `json:"name"`
 }
 
+type validationHandler struct {
+	next http.Handler
+}
+
+// this avoids possible name clashes with other packages that may be using a key
+// Name in the context
+type validationContextKey string
+
+func newValidationHandler(next http.Handler) http.Handler {
+	return validationHandler{next}
+}
+
+func (h validationHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	var request helloWorldRequest
+	decoder := json.NewDecoder(r.Body)
+
+	err := decoder.Decode(&request)
+	if err != nil {
+		log.Printf("Could not unmarshal the body to json")
+		http.Error(rw, "Bad request", http.StatusBadRequest)
+		return
+	}
+
+	c := context.WithValue(r.Context(), validationContextKey("name"), request.Name)
+	r = r.WithContext(c)
+
+	h.next.ServeHTTP(rw, r)
+}
+
+type helloWorldHandler struct{}
+
+func newHelloWorldHandler() http.Handler {
+	return helloWorldHandler{}
+}
+
+func (h helloWorldHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	name := r.Context().Value(validationContextKey("name")).(string)
+	response := helloWorldResponse{Message: "Hello " + name}
+
+	encoder := json.NewEncoder(rw)
+	encoder.Encode(response)
+}
+
 func main() {
 	port := 8080
 
-	http.HandleFunc("/hello", helloWorldHandler)
+	handler := newValidationHandler(newHelloWorldHandler())
+	http.Handle("/hello", handler)
 
 	// serve static files
 	http.Handle(
@@ -34,23 +79,4 @@ func main() {
 
 	log.Printf("Server listening on port %v\n", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
-}
-
-func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("%v /hello\n", r.Method)
-
-	var request helloWorldRequest
-	decoder := json.NewDecoder(r.Body)
-
-	err := decoder.Decode(&request)
-	if err != nil {
-		log.Printf("Could not unmarshal the body to json")
-		http.Error(w, "Bad request", http.StatusBadRequest)
-		return
-	}
-
-	response := helloWorldResponse{Message: "Hello " + request.Name}
-	encoder := json.NewEncoder(w)
-	w.Header().Set("Content-Type", "application/json")
-	encoder.Encode(&response)
 }
